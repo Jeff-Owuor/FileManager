c611301eccd77bccc95a2f4687e391a6033bfeaa
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-06-16 14:12:05 +0300

    Store data for our file manager in this file

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Video File',
+    type: 'video',
+    path: "/file-server/IMG_0263.MOV"
+  },
+  {
+    id: 2,
+    name: 'Audio File',
+    type: 'audio',
+    path: "/file-server/Donholm Road 2.m4a"
+  },
+  {
+    id: 3,
+    name: 'Document File',
+    type: 'document',
+    path: "/file-server/resume (3).pdf"
+  },
+  {
+    id: 4,
+    name: 'Image File',
+    type: 'image',
+    path: "/file-server/IMG_0260.jpg"
+  },
+   {
+    id: 5,
+    name: 'Video File',
+    type: 'video',
+    path: "/file-server/IMG_0265.MOV"
+  },
+]
